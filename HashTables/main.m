7d10a62dc0cc7209c2b59fb366a263783c48b2e5
Daniel Haaser <thedapperdan@gmail.com>
2014-11-25 11:14:36 -0800

    Initial commit

---
+++ HashTables/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HashTables
+//
+//  Created by Daniel Haaser on 11/25/14.
+//  Copyright (c) 2014 MakeSchool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
