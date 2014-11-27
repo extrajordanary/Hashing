0cd2419f9349241eb3ccccf0014979e10a8c94ba
Jordan Arnesen <jordan101112@gmail.com>
2014-11-26 17:43:08 -0800

    works in both chained linked list version and linear probe version

--- HashTables/HashTable.m
+++ HashTables/HashTable.m
@@ -9,10 +9,14 @@
 #import "HashTable.h"
 #import "KeyValueLinkedList.h"
 
-static const unsigned int kBucketCount = 128;
+static const unsigned int kBucketCount = 12800;
+static const BOOL isChained = NO;
 
 @implementation HashTable {
     NSMutableArray *buckets;
+    int entryCount;
+    int bucketCount;
+    float loadFactor;
 }
 
 #pragma mark - Lifecycle
@@ -23,9 +27,14 @@ static const unsigned int kBucketCount = 128;
     
     if (self)
     {
-        buckets = [[NSMutableArray alloc] initWithCapacity:kBucketCount];
-        for(int i = 0; i < kBucketCount; i++) {
-            buckets[i] = [[KeyValueLinkedList alloc] init];
+        bucketCount = kBucketCount;
+        buckets = [[NSMutableArray alloc] initWithCapacity:bucketCount];
+        for(int i = 0; i < bucketCount; i++) {
+            if (isChained) {
+                buckets[i] = [[KeyValueLinkedList alloc] init]; // chained version
+            } else {
+                buckets[i] = [NSNull null]; // probed version
+            }
         }
     }
     
@@ -35,24 +44,31 @@ static const unsigned int kBucketCount = 128;
 #pragma mark - Get and Set and Remove Objects
 
 - (void)setObject:(id)object forKey:(NSString*)key {
-//    unsigned int hash = [self hashForString:key];
-//    unsigned int bucket = hash % kBucketCount;
-//    NSLog(@"\nHASH: %lu \nBUCKET: %lu", (unsigned long)hash, (unsigned long)bucket);
-
-//    KeyValueLinkedList *linkedList = [self bucketListForKey:key];
-    
-    [[self bucketListForKey:key] addObject:object forKey:key];
+    if (isChained) {
+        [[self chainedBucketListForKey:key] addObject:object forKey:key];
+    } else {
+        [self probedSetObject:object forKey:key];
+    }
+    entryCount++;
+    [self updateLoadFactor];
 }
 
 - (id)objectForKey:(NSString*)key {
-//    unsigned int hash = [self hashForString:key];
-//    unsigned int bucket = hash % kBucketCount;
-//    KeyValueLinkedList *linkedList = buckets[bucket];
-    return [[self bucketListForKey:key] objectForKey:key];
- }
+    if (isChained) {
+        return [[self chainedBucketListForKey:key] objectForKey:key];
+    } else {
+        return [self probedGetObjectForKey:key];
+    }
+}
 
 - (void)removeObjectForKey:(NSString*)key {
-    [[self bucketListForKey:key] removeObjectForKey:key];
+    if (isChained) {
+    [[self chainedBucketListForKey:key] removeObjectForKey:key];
+    } else {
+        [self probedRemoveObjectForKey:key];
+    }
+    entryCount--;
+    [self updateLoadFactor];
 }
 
 
@@ -74,10 +90,124 @@ static const unsigned int kBucketCount = 128;
     return hash;
 }
 
--(KeyValueLinkedList*) bucketListForKey:(NSString*)key {
+-(KeyValueLinkedList*) chainedBucketListForKey:(NSString*)key {
+    unsigned int hash = [self hashForString:key];
+    unsigned int bucket = hash % bucketCount;
+    
+    return (KeyValueLinkedList*)buckets[bucket];
+}
+
+-(void)updateLoadFactor {
+    loadFactor = entryCount / bucketCount;
+}
+
+-(void)checkLoadFactor {
+    if (loadFactor > 0.8) {
+        // expand size of table
+        
+        // rehash all the things
+    }
+}
+#pragma mark - Probed Versions
+-(void) probedSetObject:(id)object forKey:(NSString*)key {
+    // get hash and target bucket
     unsigned int hash = [self hashForString:key];
     unsigned int bucket = hash % kBucketCount;
-    return (KeyValueLinkedList*)buckets[bucket];
+    
+    // create list with object to add
+    KeyValueLinkedList *linkedList = [[KeyValueLinkedList alloc] init];
+    [linkedList addObject:object forKey:key];
+
+    if (buckets[bucket]==[NSNull null]) {
+        // if yes, add list with object
+        buckets[bucket] = linkedList;
+        return;
+    }
+    // else iterate through buckets til finds an empty one
+    unsigned int i = (bucket+1) % (kBucketCount-1);
+    // until wrapping all the way around to start again
+    while (i != bucket) {
+        if (buckets[i]==[NSNull null]) {
+            // if yes, add list with object
+            buckets[i] = linkedList;
+            return;
+        }
+        // if i > kBucketCount wrap back around to 0
+        i = (i+1) % (kBucketCount-1);
+    }
+    NSLog(@"no empty bucket found");
+}
+
+-(id) probedGetObjectForKey:(NSString*)key {
+    // get hash and target bucket
+    unsigned int hash = [self hashForString:key];
+    unsigned int bucket = hash % bucketCount;
+    
+    id object;
+    if ([buckets[bucket] isKindOfClass:[KeyValueLinkedList class]]) {
+        // if bucket has a list, check for correct key
+        object = [(KeyValueLinkedList*)buckets[bucket] objectForKey:key];
+        if (object) {
+            return object;
+        }
+    } else {
+        // target must be nil from being deleted, so return nil
+        return nil;
+    }
+
+    // else iterate through buckets til finds an empty one
+    unsigned int i = (bucket+1) % (bucketCount-1);
+    // until wrapping all the way around to start again
+    while (i != bucket) {
+        if ([buckets[i] isKindOfClass:[KeyValueLinkedList class]]) {
+            // if yes, add list with object
+            object = [(KeyValueLinkedList*)buckets[i] objectForKey:key];
+            if (object) {
+                return object;
+            }
+        } else {
+            // target must be nil from being deleted, so return nil
+            return nil;
+        }
+        // if i > kBucketCount wrap back around to 0
+        i = (i+1) % (bucketCount-1);
+    }
+//    NSLog(@"object not found");
+    return nil;
+}
+
+-(void) probedRemoveObjectForKey:(NSString*)key {
+    // get hash and target bucket
+    unsigned int hash = [self hashForString:key];
+    unsigned int bucket = hash % bucketCount;
+    
+    id object;
+    if ([buckets[bucket] isKindOfClass:[KeyValueLinkedList class]]) {
+        // if bucket has a list, check for correct key
+        object = [(KeyValueLinkedList*)buckets[bucket] objectForKey:key];
+        if (object) {
+            buckets[bucket] = [NSNull null];
+//            [(KeyValueLinkedList*)buckets[bucket] removeObjectForKey:key];
+            return;
+        }
+    }
+    
+    // else iterate through buckets til finds an empty one
+    unsigned int i = (bucket+1) % (bucketCount-1);
+    // until wrapping all the way around to start again
+    while (i != bucket) {
+        if ([buckets[i] isKindOfClass:[KeyValueLinkedList class]]) {
+            // if yes, add list with object
+            object = [(KeyValueLinkedList*)buckets[i] objectForKey:key];
+            if (object) {
+                buckets[bucket] = [NSNull null];
+                return;
+            }
+        }
+        // if i > kBucketCount wrap back around to 0
+        i = (i+1) % (bucketCount-1);
+    }
+    NSLog(@"object to remove not found");
 }
 
 @end
